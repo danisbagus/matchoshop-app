e89b95c2ecd791cf26f8c53755f9eeb6f7d5d118
danisbagus <danisbagussetiawan@gmail.com>
2022-03-03 22:04:19 +0700

    update proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api", // You can pass in an array too eg. ['/api', '/another/path']
+    ["/api", "/auth"], // You can pass in an array too eg. ['/api', '/another/path']
     createProxyMiddleware({
       target: "https://matchoshop.herokuapp.com",
-      // target: "http://localhost:9000",
       changeOrigin: true,
     })
   );
